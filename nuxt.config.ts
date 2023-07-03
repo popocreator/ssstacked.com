193d77fa7984afd05691c221209838a5f72717fd
popomon <popomonclub@gmail.com>
2023-07-03 21:34:06 +0900

    [WIP]: Applied architecture

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+	devtools: {enabled: true},
+	// Using package name (recommended usage)
+	// '@nuxtjs/example',
+
+	// Load a local module
+	// './modules/example',
+
+	// Add module with inline-options
+	// ['./modules/example', { token: '123' }]
+
+	// Inline module definition
+	// async (inlineOptions, nuxt) => { }
+
+	// Module package alias (require)
+	// alias: {
+	//   'sample-library': 'sample-library/dist/sample-library.cjs.js'
+	// }
+});
