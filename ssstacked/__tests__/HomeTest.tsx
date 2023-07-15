468377c97c6dca98dc3ee4b40b41c28342afb21c
popomon <popomonclub@gmail.com>
2023-07-15 20:22:13 +0900

    fix: Fixed test code alias

--- ssstacked/__tests__/HomeTest.tsx
+++ ssstacked/__tests__/HomeTest.tsx
@@ -4,7 +4,7 @@
 
 import {render, screen} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import Home from '../src/app/page';
+import Home from '@/pages';
 
 test('Check for Getting Started Text', () => {
 	const {getByText} = render(<Home />);
