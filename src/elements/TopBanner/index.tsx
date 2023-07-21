ac16e49fea96fbb794d7cbc522da7f64eb5afe36
popomon <popomonclub@gmail.com>
2023-07-22 00:50:08 +0900

    Fixed image path

--- src/elements/TopBanner/index.tsx
+++ src/elements/TopBanner/index.tsx
@@ -1,10 +1,12 @@
+import Image from 'next/image';
 import styles from './index.module.css';
 import React from 'react';
+import UkrainianFlag from '/public/images/ukrainian-flag.png';
 
 export default function TopBanner() {
 	return (
 		<section className={styles.topBanner}>
-			<img src={'/ukrainian-flag.png'} alt={'Ukraine Flag'} />
+			<Image src={UkrainianFlag} alt={'Ukraine Flag'} />
 			<p className={styles.test}>
 				Stop the War.{' '}
 				<span className={styles.topBannerText_desktop}>
