1e08caf7bcb28cb17b5a5321301d69c3d8505053
popomon <popomonclub@gmail.com>
2023-09-12 00:38:56 +0900

    Modified deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,5 @@ yarn build
 sudo mkdir -p /var/www/ssstacked.com
 sudo rm -rf /var/www/ssstacked.com/.next
 sudo cp -r .next /var/www/ssstacked.com/.next
-yarn start
+pm2 start "yarn start"
+# yarn start
