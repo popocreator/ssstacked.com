bfe869351779bfa865e3e2a2addd63eb8b08a245
popomon <popomonclub@gmail.com>
2023-09-13 02:34:34 +0900

    Set process name

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 # Ubuntu
+pm2 stop ssstacked
 yarn build
 sudo mkdir -p /var/www/ssstacked.com
 sudo rm -rf /var/www/ssstacked.com/.next
 sudo cp -r .next /var/www/ssstacked.com/.next
-pm2 start "yarn start"
+pm2 start "yarn start" --name ssstacked
+
 # yarn start
