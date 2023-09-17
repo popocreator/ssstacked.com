8ff0d07992c8189d2cd05dc4072e26cdfd19a037
popomon <popomonclub@gmail.com>
2023-09-17 17:47:21 +0900

    [WIP]: Updated nginx script

--- lightsail/_nginx.sh
+++ lightsail/_nginx.sh
@@ -3,6 +3,13 @@ sudo amazon-linux-extras install nginx1
 nginx -v
 
 # Start NGINX
+sudo fuser -k 80/tcp
 sudo systemctl enable nginx
 sudo systemctl start nginx
 sudo systemctl status nginx
+
+# /etc/XXX/nginx.conf 수정
+
+# server_name "_" -> ssstacked.com
+
+# certbot 인증서 생성 명령어 실행
